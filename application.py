5fbad3a8315e1b704143e9294e893902cd41854a
Paron Sarampakhul <paronsrpk@gmail.com>
2019-04-13 20:33:25 +0700

    Init

---
+++ application.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, url_for, request, redirect, flash, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+app = Flask(__name__)
+
+
+if __name__ == '__main__':
+	app.secret_key = 'super_secret_key'
+	app.debug = True
+	app.run(host='0.0.0.0', port=5000)
